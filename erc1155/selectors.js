7ff71a3773a70d01c539eee66e50d3afaf9c4fe9
Matas Jasonas <matas.jasonas@gmail.com>
2020-09-07 23:33:00 +0300

    fix selector console.log -> console.info

--- erc1155/selectors.js
+++ erc1155/selectors.js
@@ -5,28 +5,12 @@ const path = require('path');
 const appRootPath = require('app-root-path');
 const abiCoder = require('web3-eth-abi');
 
-// const ethProvider = new providers.JsonRpcProvider(
-//   config.eth_provider,
-// );
-
-// const getAbi = () => {
-//   const json = fs.readFileSync(path.join(appRootPath.path, './erc1155/build/_ERC1155_sol_ERC1155.abi'));
-//   return JSON.parse(json.toString());
-// };
-
-// const abi = getAbi();
-
-// const contract = new ethers.Contract(
-//   config.erc1155Address,
-//   abi,
-//   ethProvider
-// );
-
 async function main () {
+  console.info('ERC1155 selectors')
   const selector = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('safeTransferFrom(address,address,bytes32,uint256,bytes)'));
-  console.log('safeTransferFrom', selector.toString().slice(0, 10));
+  console.info('safeTransferFrom: ', selector.toString().slice(0, 10));
   const test = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('transferFrom(address,address,uint256)'));
-  console.log('transferFrom', test.toString().slice(0, 10));
+  console.log('transferFrom: ', test.toString().slice(0, 10));
 }
 
 main();
