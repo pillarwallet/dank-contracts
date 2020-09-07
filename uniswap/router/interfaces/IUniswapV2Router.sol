05d86fab050cdc0511174bb07df2ee16a069151d
Matas Jasonas <matas.jasonas@gmail.com>
2020-09-08 01:53:01 +0300

    remove interface comments

--- uniswap/router/interfaces/IUniswapV2Router.sol
+++ uniswap/router/interfaces/IUniswapV2Router.sol
@@ -57,13 +57,6 @@ interface IUniswapV2Router {
         address to,
         uint deadline
     ) external returns (uint tokenAmountIn);
-    // function swapTokensForExactTokens(
-    //     uint amountOut,
-    //     uint amountInMax,
-    //     address[] calldata path,
-    //     address to,
-    //     uint deadline
-    // ) external returns (uint[] memory amounts);
 
     function quote(uint amountA, uint reserveA, uint reserveB) external pure returns (uint amountB);
     function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) external pure returns (uint amountOut);
