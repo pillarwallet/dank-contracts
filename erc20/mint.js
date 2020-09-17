ed4e9e543f70ccd3e846bfc4be30d72d5fcb3c76
Matas Jasonas <matas.jasonas@gmail.com>
2020-09-17 19:02:54 +0300

    fix mint.js eol

--- erc20/mint.js
+++ erc20/mint.js
@@ -17,8 +17,8 @@ const getAbi = () => {
 const abi = getAbi();
 const mintMethod = abi.filter(m => m.name === 'mint')[0];
 
-async function main () {
-  const amount = ethers.BigNumber.from(10).pow(18);  
+async function main() {
+  const amount = ethers.BigNumber.from(10).pow(18);
   const encodedContractFunction = abiCoder.encodeFunctionCall(
     mintMethod,
     [config.traderAddress, ethers.utils.hexlify(amount)] // 1 token of decimal 18
