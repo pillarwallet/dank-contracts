c13b9650fbb2fa20fbb3388258dfa5140c810287
Jegor Sidorenko <jegor@nfm.lt>
2020-11-26 11:45:01 +0200

    Setup hardhat

---
+++ deploy/4_uniswap.js
@@ -0,0 +1,27 @@
+const config = require('../config');
+
+/**
+ * @type import('hardhat/types').HardhatRuntimeEnvironment
+ */
+const func = async function (hre) {
+  const { deployments, deployments: { deploy }, getNamedAccounts } = hre;
+  const { deployer } = await getNamedAccounts();
+
+  const ERC20 = await deployments.get('ERC20');
+  const ERC1155 = await deployments.get('ERC1155');
+
+  const UniswapV2Factory = await deploy('UniswapV2Factory', {
+    args: [config.ownerAddress, ERC20.address, ERC1155.address],
+    from: deployer,
+    log: true,
+  });
+
+  await deploy('UniswapV2Router', {
+    args: [UniswapV2Factory.address],
+    from: deployer,
+    log: true,
+  });
+};
+module.exports = func;
+module.exports.tags = ['UNISWAP'];
+module.exports.dependencies = ['ERC20', 'ERC1155'];
---
+++ hardhat.config.js
@@ -0,0 +1,53 @@
+require('hardhat-deploy');
+require('hardhat-deploy-ethers');
+
+const mnemonic = process.env.MNEMONIC || 'test test test test test test test test test test test junk';
+const accounts = mnemonic ? { mnemonic } : undefined;
+
+const setupInfura = (networkName, chainId, accounts) => ({
+  url: `https://${networkName}.infura.io/v3/${process.env.INFURA_TOKEN}`,
+  chainId,
+  accounts,
+})
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const config = {
+  namedAccounts: {
+    deployer: 0,
+  },
+  networks: {
+    hardhat: {
+      accounts,
+      chainId: 9999,
+    },
+    localhost: {
+      url: 'http://localhost:8545',
+      accounts,
+    },
+    mainnet: setupInfura('mainnet', 1),
+    ropsten: setupInfura('ropsten', 3),
+    rinkeby: setupInfura('rinkeby', 4),
+    kovan: setupInfura('kovan', 42),
+    goerli: setupInfura('goerli', 5),
+  },
+  solidity: {
+    version: '0.6.12',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 2000,
+      },
+    },
+  },
+  paths: {
+    sources: 'src',
+    cache: './.hardhat/cache',
+    artifacts: './.hardhat/artifacts',
+    deploy: './deploy',
+    deployments: './.hardhat/deployments',
+  },
+};
+
+module.exports = config;
