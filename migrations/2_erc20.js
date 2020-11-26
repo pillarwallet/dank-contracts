9cf69253ab868d7ca06616a4ed1de83bd9c85707
Jegor Sidorenko <jegor@nfm.lt>
2020-11-26 18:45:27 +0200

    Remove truffle config and migration files

--- migrations/2_erc20.js
+++
@@ -1,9 +0,0 @@
-const ERC20 = artifacts.require('ERC20');
-
-module.exports = async (deployer, network) => {
-  if (network === 'test') {
-    return;
-  }
-
-  await deployer.deploy(ERC20, 'Stonk', 'STNK');
-};
--- migrations/3_erc721.js
+++
@@ -1,9 +0,0 @@
-const ERC721 = artifacts.require('ERC721');
-
-module.exports = async (deployer, network) => {
-  if (network === 'test') {
-    return;
-  }
-
-  await deployer.deploy(ERC721, 'SAILOR_MOON_WAND', 'SMW');
-};
