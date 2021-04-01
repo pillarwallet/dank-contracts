dd64791e41ce034dc11b2361d5b68aae46c0c0b3
Jegor Sidorenko <jegor@nfm.lt>
2021-04-01 21:10:43 +0300

    Transfer ownership of the bridge contract

--- deploy/005_bridge.js
+++ deploy/005_bridge.js
@@ -3,8 +3,9 @@
  */
 const func = async function (hre) {
   const {
-    deployments: { deploy },
+    deployments: { deploy, execute },
     getNamedAccounts,
+    network: { name: networkName },
   } = hre;
   const { deployer } = await getNamedAccounts();
 
@@ -13,6 +14,22 @@ const func = async function (hre) {
     from: deployer,
     log: true,
   });
+
+  let owner;
+  switch (networkName) {
+    case 'goerli':
+    case 'rinkeby':
+      owner = '0xaDb2Ce8343Ea0cEB3828AD1C7788BCbE2dfcb76D';
+      break;
+    case 'mainnet':
+    case 'bsc':
+      owner = '0x426435a8452733858771dB2E674b613813BA70CA';
+      break;
+    default:
+      owner = '0x5cCA0E658c05482a4569c12487E3D6e51Fe28b28';
+  }
+
+  await execute('Bridge', { from: deployer }, 'transferOwnership', owner);
 };
 module.exports = func;
 module.exports.tags = ['Bridge'];
